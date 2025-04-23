3c99b24912cb912fbcc8a4a845dcc2ffb26d16e8
rajat20040 <158429104+rajat20040@users.noreply.github.com>
2025-04-23 10:04:21 +0530

    heightofbinarytree.java

---
+++ heightofbinarytree.java
@@ -0,0 +1,40 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        Solution solution = new Solution();
+        int height = solution.maxDepth(root);
+
+        System.out.println("Height of the binary tree: " + height);
+    }
+}
