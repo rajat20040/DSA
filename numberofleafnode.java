87b8424e06bfa9f83753293e4d467c01b66c7e10
rajat20040 <158429104+rajat20040@users.noreply.github.com>
2025-04-23 10:05:33 +0530

    numberofleafnode.java

---
+++ numberofleafnode.java
@@ -0,0 +1,40 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Solution {
+    public int countLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        return countLeaves(root.left) + countLeaves(root.right);
+    }
+}
+
+
+public class main {
+    public static void main(String[] args) {
+   
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        Solution solution = new Solution();
+        int leafCount = solution.countLeaves(root);
+
+        System.out.println("Number of leaf nodes: " + leafCount);
+    }
+}
